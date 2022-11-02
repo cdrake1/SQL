38a9360eb60996dc09ef2484e5cb7e1d6bda7946
Collin Drakegit config --global user.email Collin.Drake1@marist.edugit config --global user.name Collin <Collin.Drake1@marist.edu>
2022-11-02 12:17:49 -0400

    TV2 DONE

--- SQLTVlab2/TV2Query6.sql
+++ SQLTVlab2/TV2Query6.sql
@@ -1,6 +1,4 @@
 CREATE OR REPLACE VIEW TV2Query6 AS
-SELECT zspinoff.spinoff_num, zreceives.award_name, ztvshow.show_name
-FROM zspinoff FULL JOIN ztvshow 
-ON zpsinoff.parent_num = ztvshow.show_num
-FULL JOIN zreceives
+SELECT zreceives.awardid, ztvshow.show_name
+FROM zreceives FULL JOIN ztvshow 
 ON ztvshow.show_num = zreceives.show_num;
